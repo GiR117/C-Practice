0dcfcae0b342ca2db6959f3efff00cbc2cfb3e58
WoodsWolf117 <timmcmackenjr@gmail.com>
2016-07-16 16:33:13 -0700

    Wrote a simple hello world program in C.

--- ex1.c
+++ ex1.c
@@ -2,6 +2,10 @@
 int main(int argc, char *argv[])
 {
   puts("Hello world.");
-
+  puts("It's a beautiful day.");
+  puts("A nice afternoon for learning.");
+  puts("Good luck with your code");
+  puts("Huzzah");
+  
   return 0;
 }
